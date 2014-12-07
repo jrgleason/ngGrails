766dfb0eff91e5291d4df9d8e7eeeed706df61ee
Your Name <jackiegleason@gmail.com>
2014-12-07 18:55:44 -0500

    Fixing some other issues I found

--- grails-app/controllers/org/gleason/test/QuestionController.groovy
+++ grails-app/controllers/org/gleason/test/QuestionController.groovy
@@ -13,8 +13,15 @@ class QuestionController {
 		render questionService.get(params.id) as JSON
 	}
 	def update() {
-		def obj = (Question) JSON.parse(request.reader.text);
-		render questionService.update(obj) as JSON
+	    // TODO: I would like this to be more like this, but it isn't working and I just need things working.
+//		def obj = (Question)JSON.parse(request.reader.text);
+		def obj = JSON.parse(request.reader.text);
+		Question question = new Question();
+		question.key = obj.key
+		question.text = obj.text
+		question.desc = obj.desc
+		question.voteCount = obj.voteCount
+		render questionService.update(question) as JSON
 	}
 	def create(Question q){
 		render questionService.create(q) as JSON
