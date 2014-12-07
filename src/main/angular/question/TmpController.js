3202d0012232289647ce0960ed5999f1f14c2860
Your Name <jackiegleason@gmail.com>
2014-12-07 11:21:06 -0500

    Cleaning

--- src/main/angular/question/TmpController.js
+++
@@ -1,7 +0,0 @@
-function TempController($scope, $routeParams){
-	$scope.tmpCtrl = this;
-	this.id = $routeParams.id
-	console.log("The id is "+ this.id);
-}
-angular.module('jg.ngGrails')
-.controller('TmpController', TempController);
