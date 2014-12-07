6b3c4feb3f8b690a1462d965da1189def86c3f83
Your Name <jackiegleason@gmail.com>
2014-12-07 11:29:28 -0500

    Doing more cleaning

--- src/main/angular/navigation/NavigationService.js
+++
@@ -1,7 +0,0 @@
-function NavigationService($location){
-	this.go = function (path) {
-        $location.path(path);
-    }
-}
-angular.module('jg.ngGrails')
-       .service('navService', NavigationService);
