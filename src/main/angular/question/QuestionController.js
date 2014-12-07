2e701915dc947df4384798d8ff9602b06fb3b9b9
Your Name <jackiegleason@gmail.com>
2014-12-07 11:15:12 -0500

    Alice helped me to finish the editing portion

--- src/main/angular/question/QuestionController.js
+++ src/main/angular/question/QuestionController.js
@@ -2,12 +2,15 @@ function QuestionController($scope, $location, questionService) {
 	$scope.questionCtrl = this;
 	var _this = this,
 	    question = questionService.question
-	
 	questionService.getQuestions();    
     this.questions = questionService.questions;
-	$scope.questions = questionService.questions;
+    this.selectedId = questionService.selectedQuestion;
 	this.selectedQuestion = questionService.questions[questionService.selectedQuestion];
-	
+	$scope.$watch('questionCtrl.questions' , function(val){
+		if(val != null && questionService.selectedQuestion != null){
+			this.selectedQuestion = questionService.questions[questionService.selectedQuestion];
+		}
+	})
 	this.gotoEdit = function(key) {
 		questionService.selectedQuestion = key
 		$location.path("/edit");
@@ -18,12 +21,12 @@ function QuestionController($scope, $location, questionService) {
 		$location.path('');
 	}
 	this.voteUp = function(index) {
-		var questionToUpdate = _this.questions[index]
+		var questionToUpdate = _this.questions[index];
 		questionToUpdate.voteCount++;
 		questionToUpdate.put();
 	}
 	this.voteDown = function(index) {
-		var questionToUpdate = _this.questions[index]
+		var questionToUpdate = _this.questions[index];
 		questionToUpdate.voteCount--;
 		questionToUpdate.put();
 	}
@@ -32,8 +35,10 @@ function QuestionController($scope, $location, questionService) {
 		questionToUpdate.remove();
 	}
 	this.edit = function(index){
-		var questionToUpdate = _this.questions[index]
+		var questionToUpdate = questionService.questions[index];
+		console.log("Updating Question "+ (index==null)+" "+this.selectedId+" "+questionService.selectedQuestion)
 		questionToUpdate.put();
+		$location.path('');
 	}
 }
 angular.module('jg.ngGrails').controller('questionController',
